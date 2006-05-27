7566d04e54f91d4b046ae08a0cd61b13c843bf2f
Jean-François Wauthy <pollux@xfce.org>
2006-05-27 19:17:58 +0000

    update copyright info
    (Old svn revision: 4283)

--- xfburn/xfburn-file-browser.h
+++ xfburn/xfburn-file-browser.h
@@ -1,6 +1,6 @@
 /* $Id$ */
 /*
- * Copyright (c) 2005 Jean-François Wauthy (pollux@xfce.org)
+ * Copyright (c) 2005-2006 Jean-François Wauthy (pollux@xfce.org)
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
