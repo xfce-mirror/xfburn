4f99468f6ec8e8fed530efb042dd68587e637990
David Mohr <squisher@xfce.org>
2009-08-18 18:50:03 -0600

    Compute languages on the fly instead of using LINGUAS

--- autogen.sh
+++ autogen.sh
@@ -18,17 +18,8 @@ EOF
   exit 1
 }
 
-# verify that po/LINGUAS is present
-(test -f po/LINGUAS) >/dev/null 2>&1 || {
-  cat >&2 <<EOF
-autogen.sh: The file po/LINGUAS could not be found. Please check your snapshot
-            or try to checkout again.
-EOF
-  exit 1
-}
-
 # substitute revision and linguas
-linguas=`sed -e '/^#/d' po/LINGUAS`
+linguas=`ls po/*.po | awk 'BEGIN { FS="[./]"; ORS=" " } { print $2 }'`
 
 if [ -d .git ]; then
   revision=`git rev-parse --short HEAD 2>/dev/null`
