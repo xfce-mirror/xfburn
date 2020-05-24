32348eb121d0509f656e29e61c511df0ebe8f35f
Romain Bouvier <skunnyk@alteroot.org>
2020-05-24 16:44:22 +0200

    Modernize build system
    - Remove useless configurations
    - Correctly use XDT_FEATURE_DEBUG when building from git
    - Fix autohell warnings
    - Switch to foreign mode in AM_INIT_AUTOMAKE (README is not mandatory
      anymore)

--- autogen.sh
+++ autogen.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 #
-# $Id: autogen.sh 13564 2005-04-12 15:03:11Z jasper $
-#
-# Copyright (c) 2002-2005
+# Copyright (c) 2002-2020
 #         The Xfce development team. All rights reserved.
 #
 # Written for Xfce by Benedikt Meurer <benny@xfce.org>.
@@ -13,30 +11,11 @@
 autogen.sh: You don't seem to have the Xfce development tools installed on
             your system, which are required to build this software.
             Please install the xfce4-dev-tools package first, it is available
-            from http://www.xfce.org/.
+            from your distribution or https://www.xfce.org/.
 EOF
   exit 1
 }
 
-# substitute revision and linguas
-linguas=`ls po/*.po | awk 'BEGIN { FS="[./]"; ORS=" " } { print $2 }'`
-
-if [ -d .git ]; then
-  revision=`git rev-parse --short HEAD 2>/dev/null`
-fi
-
-if [ -z "$revision" ]; then
-  revision="UNKNOWN"
-fi
-
-sed -e "s/@LINGUAS@/${linguas}/g" \
-    -e "s/@REVISION@/${revision}/g" \
-    < "configure.ac.in" > "configure.ac"
-
-if test -f autogen.configureflags; then
-  configureflags=`cat autogen.configureflags`
-fi
-
-exec xdt-autogen $configureflags $@
+exec xdt-autogen $@
 
 # vi:set ts=2 sw=2 et ai:
