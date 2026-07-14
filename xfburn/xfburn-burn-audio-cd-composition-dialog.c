605a9b533dbdaac2af27918d07e354dbcf5aac84
Gaël Bonithon <gael@xfce.org>
2026-07-14 06:58:39 +0200

    gcc-analyzer: Fix -Wanalyzer-null-dereference

--- xfburn/xfburn-burn-audio-cd-composition-dialog.c
+++ xfburn/xfburn-burn-audio-cd-composition-dialog.c
@@ -274,6 +274,8 @@ thread_burn_composition (ThreadBurnCompositionParams * params)
 
   struct burn_drive_info *drive_info = NULL;
 
+  g_return_val_if_fail (params->tracks != NULL, NULL);
+
   disc = burn_disc_create ();
   session = burn_session_create ();
 
