xfce-mirror/xfburn

commit 29beb550ae7f56c3cde96da7f1330449df292200

Normalize type declaration/definition

xfburn/xfburn-blank-dialog.h
@@ -26,24 +26,9 @@
 
 G_BEGIN_DECLS
 
-#define XFBURN_TYPE_BLANK_DIALOG         (xfburn_blank_dialog_get_type ())
-#define XFBURN_BLANK_DIALOG(o)           (G_TYPE_CHECK_INSTANCE_CAST ((o), XFBURN_TYPE_BLANK_DIALOG, XfburnBlankDialog))
-#define XFBURN_BLANK_DIALOG_CLASS(k)     (G_TYPE_CHECK_CLASS_CAST((k), XFBURN_TYPE_BLANK_DIALOG, XfburnBlankDialogClass))
-#define XFBURN_IS_BLANK_DIALOG(o)        (G_TYPE_CHECK_INSTANCE_TYPE ((o), XFBURN_TYPE_BLANK_DIALOG))
-#define XFBURN_IS_BLANK_DIALOG_CLASS(k)  (G_TYPE_CHECK_CLASS_TYPE ((k), XFBURN_TYPE_BLANK_DIALOG))
-#define XFBURN_BLANK_DIALOG_GET_CLASS(o) (G_TYPE_INSTANCE_GET_CLASS ((o), XFBURN_TYPE_BLANK_DIALOG, XfburnBlankDialogClass))
+#define XFBURN_TYPE_BLANK_DIALOG (xfburn_blank_dialog_get_type ())
+G_DECLARE_FINAL_TYPE (XfburnBlankDialog, xfburn_blank_dialog, XFBURN, BLANK_DIALOG, XfceTitledDialog)
 
-typedef struct
-{
-  XfceTitledDialog parent;
-} XfburnBlankDialog;
-
-typedef struct
-{
-  XfceTitledDialogClass parent_class;
-} XfburnBlankDialogClass;
-
-GType xfburn_blank_dialog_get_type (void);
 GtkWidget *xfburn_blank_dialog_new (void);
 GtkWidget * xfburn_blank_dialog_new_eject (gboolean eject);
 

xfburn/xfburn-burn-audio-cd-composition-dialog.h
@@ -26,30 +26,14 @@
 
 G_BEGIN_DECLS
 
-#define XFBURN_TYPE_BURN_AUDIO_CD_COMPOSITION_DIALOG         (xfburn_burn_audio_cd_composition_dialog_get_type ())
-#define XFBURN_BURN_AUDIO_CD_COMPOSITION_DIALOG(o)           (G_TYPE_CHECK_INSTANCE_CAST ((o), XFBURN_TYPE_BURN_AUDIO_CD_COMPOSITION_DIALOG, XfburnBurnAudioCdCompositionDialog))
-#define XFBURN_BURN_AUDIO_CD_COMPOSITION_DIALOG_CLASS(k)     (G_TYPE_CHECK_CLASS_CAST((k), XFBURN_TYPE_BURN_AUDIO_CD_COMPOSITION_DIALOG, XfburnBurnAudioCdCompositionDialogClass))
-#define XFBURN_IS_BURN_AUDIO_CD_COMPOSITION_DIALOG(o)        (G_TYPE_CHECK_INSTANCE_TYPE ((o), XFBURN_TYPE_BURN_AUDIO_CD_COMPOSITION_DIALOG))
-#define XFBURN_IS_BURN_AUDIO_CD_COMPOSITION_DIALOG_CLASS(k)  (G_TYPE_CHECK_CLASS_TYPE ((k), XFBURN_TYPE_BURN_AUDIO_CD_COMPOSITION_DIALOG))
-#define XFBURN_BURN_AUDIO_CD_COMPOSITION_DIALOG_GET_CLASS(o) (G_TYPE_INSTANCE_GET_CLASS ((o), XFBURN_TYPE_BURN_AUDIO_CD_COMPOSITION_DIALOG, XfburnBurnAudioCdCompositionDialogClass))
+#define XFBURN_TYPE_BURN_AUDIO_CD_COMPOSITION_DIALOG (xfburn_burn_audio_cd_composition_dialog_get_type ())
+G_DECLARE_FINAL_TYPE (XfburnBurnAudioCdCompositionDialog, xfburn_burn_audio_cd_composition_dialog, XFBURN, BURN_AUDIO_CD_COMPOSITION_DIALOG, XfceTitledDialog)
 
 enum {
   XFBURN_BURN_AUDIO_CD_COMPOSITION_DIALOG_CANCEL = -1,
   XFBURN_BURN_AUDIO_CD_COMPOSITION_DIALOG_BURN,
   XFBURN_BURN_AUDIO_CD_COMPOSITION_DIALOG_BLANK,
 };
-
-typedef struct
-{
-  XfceTitledDialog parent;
-} XfburnBurnAudioCdCompositionDialog;
-
-typedef struct
-{
-  XfceTitledDialogClass parent_class;
-} XfburnBurnAudioCdCompositionDialogClass;
-
-GType xfburn_burn_audio_cd_composition_dialog_get_type (void);
 
 GtkWidget *xfburn_burn_audio_cd_composition_dialog_new (GSList *tracklist);
 

xfburn/xfburn-burn-data-composition-base-dialog.h
@@ -26,12 +26,8 @@
 
 G_BEGIN_DECLS
 
-#define XFBURN_TYPE_BURN_DATA_COMPOSITION_BASE_DIALOG         (xfburn_burn_data_composition_base_dialog_get_type ())
-#define XFBURN_BURN_DATA_COMPOSITION_BASE_DIALOG(o)           (G_TYPE_CHECK_INSTANCE_CAST ((o), XFBURN_TYPE_BURN_DATA_COMPOSITION_BASE_DIALOG, XfburnBurnDataCompositionBaseDialog))
-#define XFBURN_BURN_DATA_COMPOSITION_BASE_DIALOG_CLASS(k)     (G_TYPE_CHECK_CLASS_CAST((k), XFBURN_TYPE_BURN_DATA_COMPOSITION_BASE_DIALOG, XfburnBurnDataCompositionBaseDialogClass))
-#define XFBURN_IS_BURN_DATA_COMPOSITION_BASE_DIALOG(o)        (G_TYPE_CHECK_INSTANCE_TYPE ((o), XFBURN_TYPE_BURN_DATA_COMPOSITION_BASE_DIALOG))
-#define XFBURN_IS_BURN_DATA_COMPOSITION_BASE_DIALOG_CLASS(k)  (G_TYPE_CHECK_CLASS_TYPE ((k), XFBURN_TYPE_BURN_DATA_COMPOSITION_BASE_DIALOG))
-#define XFBURN_BURN_DATA_COMPOSITION_BASE_DIALOG_GET_CLASS(o) (G_TYPE_INSTANCE_GET_CLASS ((o), XFBURN_TYPE_BURN_DATA_COMPOSITION_BASE_DIALOG, XfburnBurnDataCompositionBaseDialogClass))
+#define XFBURN_TYPE_BURN_DATA_COMPOSITION_BASE_DIALOG (xfburn_burn_data_composition_base_dialog_get_type ())
+G_DECLARE_DERIVABLE_TYPE (XfburnBurnDataCompositionBaseDialog, xfburn_burn_data_composition_base_dialog, XFBURN, BURN_DATA_COMPOSITION_BASE_DIALOG, XfceTitledDialog)
 
 enum {
   XFBURN_BURN_DATA_COMPOSITION_DIALOG_CANCEL = -1,
@@ -39,17 +35,10 @@ enum {
   XFBURN_BURN_DATA_COMPOSITION_DIALOG_BLANK,
 };
 
-typedef struct
-{
-  XfceTitledDialog parent;
-} XfburnBurnDataCompositionBaseDialog;
-
-typedef struct
+struct _XfburnBurnDataCompositionBaseDialogClass
 {
   XfceTitledDialogClass parent_class;
-} XfburnBurnDataCompositionBaseDialogClass;
-
-GType xfburn_burn_data_composition_base_dialog_get_type (void);
+};
 
 GtkWidget *xfburn_burn_data_composition_base_dialog_new (IsoImage *image);
 

xfburn/xfburn-burn-image-dialog.h
@@ -27,24 +27,9 @@
 
 G_BEGIN_DECLS
 
-#define XFBURN_TYPE_BURN_IMAGE_DIALOG         (xfburn_burn_image_dialog_get_type ())
-#define XFBURN_BURN_IMAGE_DIALOG(o)           (G_TYPE_CHECK_INSTANCE_CAST ((o), XFBURN_TYPE_BURN_IMAGE_DIALOG, XfburnBurnImageDialog))
-#define XFBURN_BURN_IMAGE_DIALOG_CLASS(k)     (G_TYPE_CHECK_CLASS_CAST((k), XFBURN_TYPE_BURN_IMAGE_DIALOG, XfburnBurnImageDialogClass))
-#define XFBURN_IS_BURN_IMAGE_DIALOG(o)        (G_TYPE_CHECK_INSTANCE_TYPE ((o), XFBURN_TYPE_BURN_IMAGE_DIALOG))
-#define XFBURN_IS_BURN_IMAGE_DIALOG_CLASS(k)  (G_TYPE_CHECK_CLASS_TYPE ((k), XFBURN_TYPE_BURN_IMAGE_DIALOG))
-#define XFBURN_BURN_IMAGE_DIALOG_GET_CLASS(o) (G_TYPE_INSTANCE_GET_CLASS ((o), XFBURN_TYPE_BURN_IMAGE_DIALOG, XfburnBurnImageDialogClass))
+#define XFBURN_TYPE_BURN_IMAGE_DIALOG (xfburn_burn_image_dialog_get_type ())
+G_DECLARE_FINAL_TYPE (XfburnBurnImageDialog, xfburn_burn_image_dialog, XFBURN, BURN_IMAGE_DIALOG, XfceTitledDialog)
 
-typedef struct
-{
-  XfceTitledDialog parent;
-} XfburnBurnImageDialog;
-
-typedef struct
-{
-  XfceTitledDialogClass parent_class;
-} XfburnBurnImageDialogClass;
-
-GType xfburn_burn_image_dialog_get_type (void);
 GtkWidget *xfburn_burn_image_dialog_new (void);
 void xfburn_burn_image_dialog_set_filechooser_name (GtkWidget * dialog, gchar *name);
 

xfburn/xfburn-copy-cd-dialog.h
@@ -25,24 +25,8 @@
 
 G_BEGIN_DECLS
 
-#define XFBURN_TYPE_COPY_CD_DIALOG         (xfburn_copy_cd_dialog_get_type ())
-#define XFBURN_COPY_CD_DIALOG(o)           (G_TYPE_CHECK_INSTANCE_CAST ((o), XFBURN_TYPE_COPY_CD_DIALOG, XfburnCopyCdDialog))
-#define XFBURN_COPY_CD_DIALOG_CLASS(k)     (G_TYPE_CHECK_CLASS_CAST((k), XFBURN_TYPE_COPY_CD_DIALOG, XfburnCopyCdDialogClass))
-#define XFBURN_IS_COPY_CD_DIALOG(o)        (G_TYPE_CHECK_INSTANCE_TYPE ((o), XFBURN_TYPE_COPY_CD_DIALOG))
-#define XFBURN_IS_COPY_CD_DIALOG_CLASS(k)  (G_TYPE_CHECK_CLASS_TYPE ((k), XFBURN_TYPE_COPY_CD_DIALOG))
-#define XFBURN_COPY_CD_DIALOG_GET_CLASS(o) (G_TYPE_INSTANCE_GET_CLASS ((o), XFBURN_TYPE_COPY_CD_DIALOG, XfburnCopyCdDialogClass))
-
-typedef struct
-{
-  XfceTitledDialog parent;
-} XfburnCopyCdDialog;
-
-typedef struct
-{
-  XfceTitledDialogClass parent_class;
-} XfburnCopyCdDialogClass;
-
-GType xfburn_copy_cd_dialog_get_type (void);
+#define XFBURN_TYPE_COPY_CD_DIALOG (xfburn_copy_cd_dialog_get_type ())
+G_DECLARE_FINAL_TYPE (XfburnCopyCdDialog, xfburn_copy_cd_dialog, XFBURN, COPY_CD_DIALOG, XfceTitledDialog)
 
 GtkWidget *xfburn_copy_cd_dialog_new (void);
 

xfburn/xfburn-copy-dvd-dialog.h
@@ -25,24 +25,8 @@
 
 G_BEGIN_DECLS
 
-#define XFBURN_TYPE_COPY_DVD_DIALOG         (xfburn_copy_dvd_dialog_get_type ())
-#define XFBURN_COPY_DVD_DIALOG(o)           (G_TYPE_CHECK_INSTANCE_CAST ((o), XFBURN_TYPE_COPY_DVD_DIALOG, XfburnCopyDvdDialog))
-#define XFBURN_COPY_DVD_DIALOG_CLASS(k)     (G_TYPE_CHECK_CLASS_CAST((k), XFBURN_TYPE_COPY_DVD_DIALOG, XfburnCopyDvdDialogClass))
-#define XFBURN_IS_COPY_DVD_DIALOG(o)        (G_TYPE_CHECK_INSTANCE_TYPE ((o), XFBURN_TYPE_COPY_DVD_DIALOG))
-#define XFBURN_IS_COPY_DVD_DIALOG_CLASS(k)  (G_TYPE_CHECK_CLASS_TYPE ((k), XFBURN_TYPE_COPY_DVD_DIALOG))
-#define XFBURN_COPY_DVD_DIALOG_GET_CLASS(o) (G_TYPE_INSTANCE_GET_CLASS ((o), XFBURN_TYPE_COPY_DVD_DIALOG, XfburnCopyDvdDialogClass))
-
-typedef struct
-{
-  XfceTitledDialog parent;
-} XfburnCopyDvdDialog;
-
-typedef struct
-{
-  XfceTitledDialogClass parent_class;
-} XfburnCopyDvdDialogClass;
-
-GType xfburn_copy_dvd_dialog_get_type (void);
+#define XFBURN_TYPE_COPY_DVD_DIALOG (xfburn_copy_dvd_dialog_get_type ())
+G_DECLARE_FINAL_TYPE (XfburnCopyDvdDialog, xfburn_copy_dvd_dialog, XFBURN, COPY_DVD_DIALOG, XfceTitledDialog)
 
 GtkWidget *xfburn_copy_dvd_dialog_new (void);
 

xfburn/xfburn-preferences-dialog.h
@@ -24,26 +24,8 @@
 G_BEGIN_DECLS
 
 #define XFBURN_TYPE_PREFERENCES_DIALOG         (xfburn_preferences_dialog_get_type ())
-#define XFBURN_PREFERENCES_DIALOG(o)           (G_TYPE_CHECK_INSTANCE_CAST ((o), XFBURN_TYPE_PREFERENCES_DIALOG, XfburnPreferencesDialog))
-#define XFBURN_PREFERENCES_DIALOG_CLASS(k)     (G_TYPE_CHECK_CLASS_CAST((k), XFBURN_TYPE_PREFERENCES_DIALOG, XfburnPreferencesDialogClass))
-#define XFBURN_IS_PREFERENCES_DIALOG(o)        (G_TYPE_CHECK_INSTANCE_TYPE ((o), XFBURN_TYPE_PREFERENCES_DIALOG))
-#define XFBURN_IS_PREFERENCES_DIALOG_CLASS(k)  (G_TYPE_CHECK_CLASS_TYPE ((k), XFBURN_TYPE_PREFERENCES_DIALOG))
-#define XFBURN_PREFERENCES_DIALOG_GET_CLASS(o) (G_TYPE_INSTANCE_GET_CLASS ((o), XFBURN_TYPE_PREFERENCES_DIALOG, XfburnPreferencesDialogClass))
+G_DECLARE_FINAL_TYPE (XfburnPreferencesDialog, xfburn_preferences_dialog, XFBURN, PREFERENCES_DIALOG, XfceTitledDialog)
 
-typedef struct
-{
-  XfceTitledDialog parent;
-} XfburnPreferencesDialog;
-
-typedef struct
-{
-  XfceTitledDialogClass parent_class;
-  /* Add Signal Functions Here */
-} XfburnPreferencesDialogClass;
-
-
-
-GType xfburn_preferences_dialog_get_type (void);
 GtkWidget *xfburn_preferences_dialog_new (void);
 
 G_END_DECLS

commit 8dc464830a3936fbac47d7c58c1e6322ca9e27ab

build: Add missing libxfce4ui version checks
Fixes: 29beb550ae7f56c3cde96da7f1330449df292200

xfburn/xfburn-blank-dialog.h
@@ -26,6 +26,12 @@
 
 G_BEGIN_DECLS
 
+#if !LIBXFCE4UI_CHECK_VERSION(4, 21, 8)
+#ifndef XFBURN_LIBXFCE4UI_AUTOPTR_CLEANUP_FUNC_ALREADY_DEFINED
+G_DEFINE_AUTOPTR_CLEANUP_FUNC (XfceTitledDialog, g_object_unref)
+#define XFBURN_LIBXFCE4UI_AUTOPTR_CLEANUP_FUNC_ALREADY_DEFINED 1
+#endif
+#endif
 #define XFBURN_TYPE_BLANK_DIALOG (xfburn_blank_dialog_get_type ())
 G_DECLARE_FINAL_TYPE (XfburnBlankDialog, xfburn_blank_dialog, XFBURN, BLANK_DIALOG, XfceTitledDialog)
 

xfburn/xfburn-burn-audio-cd-composition-dialog.h
@@ -26,6 +26,12 @@
 
 G_BEGIN_DECLS
 
+#if !LIBXFCE4UI_CHECK_VERSION(4, 21, 8)
+#ifndef XFBURN_LIBXFCE4UI_AUTOPTR_CLEANUP_FUNC_ALREADY_DEFINED
+G_DEFINE_AUTOPTR_CLEANUP_FUNC (XfceTitledDialog, g_object_unref)
+#define XFBURN_LIBXFCE4UI_AUTOPTR_CLEANUP_FUNC_ALREADY_DEFINED 1
+#endif
+#endif
 #define XFBURN_TYPE_BURN_AUDIO_CD_COMPOSITION_DIALOG (xfburn_burn_audio_cd_composition_dialog_get_type ())
 G_DECLARE_FINAL_TYPE (XfburnBurnAudioCdCompositionDialog, xfburn_burn_audio_cd_composition_dialog, XFBURN, BURN_AUDIO_CD_COMPOSITION_DIALOG, XfceTitledDialog)
 

xfburn/xfburn-burn-data-composition-base-dialog.h
@@ -26,6 +26,12 @@
 
 G_BEGIN_DECLS
 
+#if !LIBXFCE4UI_CHECK_VERSION(4, 21, 8)
+#ifndef XFBURN_LIBXFCE4UI_AUTOPTR_CLEANUP_FUNC_ALREADY_DEFINED
+G_DEFINE_AUTOPTR_CLEANUP_FUNC (XfceTitledDialog, g_object_unref)
+#define XFBURN_LIBXFCE4UI_AUTOPTR_CLEANUP_FUNC_ALREADY_DEFINED 1
+#endif
+#endif
 #define XFBURN_TYPE_BURN_DATA_COMPOSITION_BASE_DIALOG (xfburn_burn_data_composition_base_dialog_get_type ())
 G_DECLARE_DERIVABLE_TYPE (XfburnBurnDataCompositionBaseDialog, xfburn_burn_data_composition_base_dialog, XFBURN, BURN_DATA_COMPOSITION_BASE_DIALOG, XfceTitledDialog)
 

xfburn/xfburn-burn-image-dialog.h
@@ -27,6 +27,12 @@
 
 G_BEGIN_DECLS
 
+#if !LIBXFCE4UI_CHECK_VERSION(4, 21, 8)
+#ifndef XFBURN_LIBXFCE4UI_AUTOPTR_CLEANUP_FUNC_ALREADY_DEFINED
+G_DEFINE_AUTOPTR_CLEANUP_FUNC (XfceTitledDialog, g_object_unref)
+#define XFBURN_LIBXFCE4UI_AUTOPTR_CLEANUP_FUNC_ALREADY_DEFINED 1
+#endif
+#endif
 #define XFBURN_TYPE_BURN_IMAGE_DIALOG (xfburn_burn_image_dialog_get_type ())
 G_DECLARE_FINAL_TYPE (XfburnBurnImageDialog, xfburn_burn_image_dialog, XFBURN, BURN_IMAGE_DIALOG, XfceTitledDialog)
 

xfburn/xfburn-copy-cd-dialog.h
@@ -25,6 +25,12 @@
 
 G_BEGIN_DECLS
 
+#if !LIBXFCE4UI_CHECK_VERSION(4, 21, 8)
+#ifndef XFBURN_LIBXFCE4UI_AUTOPTR_CLEANUP_FUNC_ALREADY_DEFINED
+G_DEFINE_AUTOPTR_CLEANUP_FUNC (XfceTitledDialog, g_object_unref)
+#define XFBURN_LIBXFCE4UI_AUTOPTR_CLEANUP_FUNC_ALREADY_DEFINED 1
+#endif
+#endif
 #define XFBURN_TYPE_COPY_CD_DIALOG (xfburn_copy_cd_dialog_get_type ())
 G_DECLARE_FINAL_TYPE (XfburnCopyCdDialog, xfburn_copy_cd_dialog, XFBURN, COPY_CD_DIALOG, XfceTitledDialog)
 

xfburn/xfburn-copy-dvd-dialog.h
@@ -25,6 +25,12 @@
 
 G_BEGIN_DECLS
 
+#if !LIBXFCE4UI_CHECK_VERSION(4, 21, 8)
+#ifndef XFBURN_LIBXFCE4UI_AUTOPTR_CLEANUP_FUNC_ALREADY_DEFINED
+G_DEFINE_AUTOPTR_CLEANUP_FUNC (XfceTitledDialog, g_object_unref)
+#define XFBURN_LIBXFCE4UI_AUTOPTR_CLEANUP_FUNC_ALREADY_DEFINED 1
+#endif
+#endif
 #define XFBURN_TYPE_COPY_DVD_DIALOG (xfburn_copy_dvd_dialog_get_type ())
 G_DECLARE_FINAL_TYPE (XfburnCopyDvdDialog, xfburn_copy_dvd_dialog, XFBURN, COPY_DVD_DIALOG, XfceTitledDialog)
 

xfburn/xfburn-preferences-dialog.h
@@ -23,7 +23,13 @@
 
 G_BEGIN_DECLS
 
-#define XFBURN_TYPE_PREFERENCES_DIALOG         (xfburn_preferences_dialog_get_type ())
+#if !LIBXFCE4UI_CHECK_VERSION(4, 21, 8)
+#ifndef XFBURN_LIBXFCE4UI_AUTOPTR_CLEANUP_FUNC_ALREADY_DEFINED
+G_DEFINE_AUTOPTR_CLEANUP_FUNC (XfceTitledDialog, g_object_unref)
+#define XFBURN_LIBXFCE4UI_AUTOPTR_CLEANUP_FUNC_ALREADY_DEFINED 1
+#endif
+#endif
+#define XFBURN_TYPE_PREFERENCES_DIALOG (xfburn_preferences_dialog_get_type ())
 G_DECLARE_FINAL_TYPE (XfburnPreferencesDialog, xfburn_preferences_dialog, XFBURN, PREFERENCES_DIALOG, XfceTitledDialog)
 
 GtkWidget *xfburn_preferences_dialog_new (void);