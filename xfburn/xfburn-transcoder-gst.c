xfce-mirror/xfburn

commit 0ed33ca7a23670a09cb939db4d2f22053bb74399

Remove floating flag before releasing Gst objects
Fixes: #74

xfburn/xfburn-transcoder-gst.c
@@ -235,7 +235,7 @@ create_pipeline (XfburnTranscoderGst *trans)
 
   priv->state = XFBURN_TRANSCODER_GST_STATE_IDLE;
 
-  priv->pipeline = pipeline = gst_pipeline_new ("transcoder");
+  priv->pipeline = pipeline = g_object_ref_sink (gst_pipeline_new ("transcoder"));
 
   priv->source   = source   = gst_element_factory_make ("filesrc",       "file-source");
   priv->decoder  = decoder  = gst_element_factory_make ("decodebin",     "decoder");

commit 011b812c42db56c42dde9d1e253529353663557c

Fix previous commit
Fixes: 0ed33ca7a23670a09cb939db4d2f22053bb74399
Related: https://gitlab.xfce.org/apps/xfburn/-/merge_requests/47#note_121637

xfburn/xfburn-transcoder-gst.c
@@ -235,7 +235,10 @@ create_pipeline (XfburnTranscoderGst *trans)
 
   priv->state = XFBURN_TRANSCODER_GST_STATE_IDLE;
 
-  priv->pipeline = pipeline = g_object_ref_sink (gst_pipeline_new ("transcoder"));
+  priv->pipeline = pipeline = gst_pipeline_new ("transcoder");
+  if (pipeline != NULL) {
+    gst_object_ref_sink (pipeline);
+  }
 
   priv->source   = source   = gst_element_factory_make ("filesrc",       "file-source");
   priv->decoder  = decoder  = gst_element_factory_make ("decodebin",     "decoder");